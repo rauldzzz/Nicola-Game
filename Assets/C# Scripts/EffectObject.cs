824a6688af120563c4da7f473ee1775fb90d88b5
minewerk <92604411+minewerk@users.noreply.github.com>
2025-11-17 10:06:58 +0100

    Rhythm Game added

---
+++ Assets/C# Scripts/EffectObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EffectObject : MonoBehaviour
+{
+    public float lifetime = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
